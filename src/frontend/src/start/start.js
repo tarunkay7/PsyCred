5cbe3e2041881c0a274de92c6d57eeac190b889a
gayatrisrinivasan <gayatrisn3@gmail.com>
2024-05-16 17:03:51 +0530

    Added the blueprint for start page

--- src/frontend/src/start/start.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-        const navbarLinks = document.querySelector('.navbar-links');
-
-        hamburgerMenu.addEventListener('click', () => {
-            navbarLinks.classList.toggle('active');
-        });
